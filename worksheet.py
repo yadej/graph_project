05e455df0d3ff4c7040518022d6dc222da984e28
Tilmann Berland <tberlan@a-999903.ups.u-psud.fr>
2022-01-25 15:09:11 +0100

    copy fonc pas btw

--- worksheet.py
+++ worksheet.py
@@ -2,6 +2,8 @@ from modules.open_digraph import *
 import inspect
 
 d = open_digraph([1, 2, 3], [6], [node(0, 'a', [], [1])])
-print(d)
+print(f'd = {d}\n')
 
-print(d.copy())
+
+print(f'methodes du module open_digraph = {dir(open_digraph)}')
+
