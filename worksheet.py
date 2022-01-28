99c58e4f20fc6497b395df5254114957b6c8d77d
rui.cesista <rui.cesista@universite-paris-saclay.fr>
2022-01-28 18:57:23 +0100

    add input node et add output node

--- worksheet.py
+++ worksheet.py
@@ -4,5 +4,4 @@ import inspect
 d = open_digraph([1, 2, 3], [6], [node(0, 'a', [], [1])])
 print(f'd = {d}\n')
 
-print(f'methodes du module open_digraph = {dir(open_digraph)}')
-
+print(f'methode du module open_digraph = {dir(open_digraph)}')
