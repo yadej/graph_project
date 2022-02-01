294db9645706b00c9ca5059d5417ee2af9a66e58
Tilmann Berland <tberlan@a-999903.ups.u-psud.fr>
2022-02-01 14:30:05 +0100

    a

--- modules/matrice.py
+++ modules/matrice.py
@@ -1,7 +1,30 @@
 import random
-    
 
 random_int_list = lambda n, bound: [random.randrange(bound) for i in range(n)]
 
-def random_int_matrix(n, bound, null_diag=True):
-    return [random_int_list(n,bound) for _ in range(n)]
+def random_int_matrix(n, bound, null_diag=True, symetric=False, oriented=False, triangular=False):
+    m = [random_int_list(n,bound) for _ in range(n)]
+    
+    if symetric:
+        for i in range(n):
+            for j in range(i):
+                m[i][j] = m[j][i]
+                
+    if oriented:
+        for i in range(n):
+            for j in range(n):
+                while m[i][j] == m[j][i] and i != j:
+                    m[i][j] = random.randrange(n)
+        
+    if triangular:
+        for i in range(n):
+            for j in range(i):
+                m[j][i] = 0
+        
+    if null_diag:
+        for i in range(n):
+            m[i][i] = 0
+    
+    return m
+
+print(random_int_matrix(5, 10, oriented =True))
