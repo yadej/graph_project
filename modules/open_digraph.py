a856fdea7ca81c3126d2ebfbb01aa8915e2c24b2
Z1proW <z1prow@gmail.com>
2022-02-17 15:26:19 +0100

    reparation

--- modules/open_digraph.py
+++ modules/open_digraph.py
@@ -563,29 +563,27 @@ class open_digraph:  # for open directed graph
         
 class bool_circ(open_digraph):
 
-        def __init__(self, g=None):
-            if isinstance(g, open_digraph):
-                self.inputs = g.get_input_ids()
-                self.outputs = g.get_output_ids()
-                self.nodes = g.get_nodes()
-            if not g.is_well_formed():
-                raise Exception('is not a boolean circuit')
+    def __init__(self, g=None):
+        if isinstance(g, open_digraph):
+            super().__init__(g.get_input_ids(), g.get_output_ids(), g.get_nodes())
+        if not g.is_well_formed:
+            raise Exception('is not a boolean circuit')
 
-        def is_well_formed(self):
-            # doit etre acyclique
-            if self.is_cyclic():
+    def is_well_formed(self):
+        # doit etre acyclique
+        if self.is_cyclic():
+            return False
+
+        for n in self.get_nodes():
+            # les noeuds copie ie label='' doivent avoir un input
+            if n.get_label() == '' and len(n.get_children_ids()) != 1:
                 return False
-
-            for node in self.get_nodes():
-                # les noeuds copie ie label='' doivent avoir un input
-                if node.get_label() == '' and len(node.get_children_ids()) != 1:
+            # les noeuds ET/OU ie label='&'ou'|' doivent avoir un output
+            if n.get_label() == '&' or n.get_label() == '|':
+                if len(n.get_parent_ids()) != 1:
                     return False
-                # les noeuds ET/OU ie label='&'ou'|' doivent avoir un output
-                if node.get_label() == '&' or node.get_label() == '|':
-                    if len(node.get_parent_ids()) != 1:
-                        return False
-                # les noeuds NON ie label='~' doivent avoir un input et un output
-                if node.get_label() == '~':
-                    if len(node.get_parent_ids()) != 1 or len(node.get_children_ids()) != 1:
-                        return False
-            return True
+            # les noeuds NON ie label='~' doivent avoir un input et un output
+            if n.get_label() == '~':
+                if len(n.get_parent_ids()) != 1 or len(n.get_children_ids()) != 1:
+                    return False
+        return True
