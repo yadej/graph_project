f682dbc758c66f81043e6e71abb3919fb34fe2a5
tberlan <tilmann.berland@universite-paris-saclay.fr>
2022-03-20 00:46:00 +0100

    mieux

--- modules/open_digraph_mx/open_digraph_paths_mx.py
+++ modules/open_digraph_mx/open_digraph_paths_mx.py
@@ -78,91 +78,82 @@ class open_digraph_paths_mx:
             new = []
 
             for i in k.get_node_ids():
-                if len(k.get_node_by_id(i).get_children_ids()) == 0 and len(k.get_node_by_id(i).get_parent_ids()) != 0:
+                if not k.get_node_by_id(i).get_parent_ids() \
+                        and k.get_node_by_id(i).get_children_ids():
                     new.append(i)
 
             if not new:
-                raise Exception("Digraph cyclic")
+                raise Exception("the graph is cyclic")
 
             for i in new:
-                for a in list(k.get_node_by_id(i).get_parent_ids()):
-                    k.remove_parallel_edges((a, i))
+                for a in list(k.get_node_by_id(i).get_children_ids()):
+                    k.remove_parallel_edges((i, a))
 
             old.append(new)
             for i in k.get_node_ids():
                 if i not in visited \
-                        and len(k.get_node_by_id(i).get_children_ids()) == 0 \
-                        and len(k.get_node_by_id(i).get_parent_ids()) == 0:
+                        and not k.get_node_by_id(i).get_parent_ids() \
+                        and not k.get_node_by_id(i).get_children_ids():
                     nb += 1
                     visited.append(i)
 
-        # C'est pour ajouter la derniere ligne
+        # ajouter la derniere ligne
         flat_old = [item for t in old for item in t]
         p = [i for i in k.get_node_ids() if i not in flat_old]
         old.append(p)
-        #Retourne l'inverse d'old
-        return old[::-1]
+        return old
 
     def node_depth(self, i):
         """
         :param i: node id
         :return: depth of the node in the graph
         """
-        a = self.tri_topologique()
-
-        for depth in range(len(a)):
-            if i in a[depth]:
+        for depth, node_ids in enumerate(self.tri_topologique()):
+            if i in node_ids:
                 return depth
 
         return -1  # node not in graph
 
-    def prof_OpD(self):
+    def depth(self):
         """
         :return: max depth of digraph
         """
         return len(self.tri_topologique())
 
-    def max_dist(self, src, tgt):
+    def max_dist(self, u, v):
         """
-        :param src: id of the source node
-        :param tgt: id of the target node
+        :param u: id of the source node
+        :param v: id of the target node
         :return: maximum distance between source and target nodes
         """
-        a = list(self.get_node_ids())
-        if src not in a or tgt not in a:
-            raise Exception("Noeud qui ne sont pas dans le graph")
-        T = self.tri_topologique()
-        id1p = 0
-        id2p = 0
-        for i in range(len(T)):
-            if src in T[i]:
-                id1p = i
-            if tgt in T[i]:
-                id2p = i
-        if id1p < id2p:
-            dico = {src: 0}
-            last = tgt
-        else:
-            dico = {tgt: 0}
-            last = src
-            id1p, id2p = id2p, id1p
+        u_depth, v_depth = self.node_depth(u), self.node_depth(v)
+
+        if u_depth == -1:
+            raise Exception('u is not in the graph')
+        if v_depth == -1:
+            raise Exception('v is not in the graph')
+
+        if u_depth > v_depth:
+            u, v = v, u
+            u_depth, v_depth = v_depth, u_depth
+        dico = {u: 0}
+        last = v
+
         prev = {}
-        for i in range(id1p, id2p):
-            for j in T[i + 1]:
-                v1 = set(self.get_node_by_id(j).get_parent_ids())
-                v2 = set(dico.keys())
-                v3 = list(v1 & v2)
-                if v3:
-                    p = max(v3, key=lambda x: dico[x])
-                    dico[j] = dico[p] + 1
-                    prev[j] = p
+        T = self.tri_topologique()
+
+        for depth in range(u_depth, v_depth):
+            for i in T[depth + 1]:
+                inter = dico.keys() & self.get_node_by_id(i).get_parent_ids()
+                if inter:
+                    p = max(inter, key=lambda x: dico[x])
+                    dico[i] = dico[p] + 1
+                    prev[i] = p
+
         newprev = {}
-        if last == tgt:
-            while last != src:
-                newprev[last] = prev[last]
-                last = prev[last]
-        else:
-            while last != tgt:
-                newprev[last] = prev[last]
-                last = prev[last]
-        return max(dico[src], dico[tgt]), newprev
+
+        while last != u:
+            newprev[last] = prev[last]
+            last = prev[last]
+
+        return max(dico[u], dico[v]), newprev
