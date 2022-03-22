c61b87d7af073fd89dd8e00404294b8219494bdb
rui.cesista <rui.cesista@universite-paris-saclay.fr>
2022-03-22 15:25:24 +0100

    Exo 3 TD 8

--- modules/bool_circ.py
+++ modules/bool_circ.py
@@ -55,5 +55,17 @@ class bool_circ(open_digraph):
                 s2 = ''
             else:
                 s2 += c
+        k = 0
+        while k != len(g.get_nodes()):
+            lab = g.get_node_by_id(k).get_label()
+            new = -1
+            for i in len(k ,g.get_nodes()):
+                if g.get_node_by_id(i).get_label() == lab:
+                    new = i
+                    break
+            if new != -1:
+                g.fusion(k,new)
+            else:
+                k += 1
 
         return g
